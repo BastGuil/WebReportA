d3d207209bb221d3f0edb7200de81b90c5e2434f
spide <spidersuper2@live.fr>
2021-10-20 14:19:36 +0200

    changement app-routing.module.ts

--- Développement/src/app/app-routing.module.ts
+++ Développement/src/app/app-routing.module.ts
@@ -7,26 +7,6 @@ const routes: Routes = [
     loadChildren: () => import('./connexion/connexion.module').then( m => m.ConnexionPageModule)
   },
   {
-    path: 'superviseur',
-    loadChildren: () => import('./superviseur/superviseur.module').then( m => m.SuperviseurPageModule)
-  },
-  {
-    path: 'respo-m',
-    loadChildren: () => import('./respo-m/respo-m.module').then( m => m.RespoMPageModule)
-  },
-  {
-    path: 'respo-l',
-    loadChildren: () => import('./respo-l/respo-l.module').then( m => m.RespoLPageModule)
-  },
-  {
-    path: 'respo-p',
-    loadChildren: () => import('./respo-p/respo-p.module').then( m => m.RespoPPageModule)
-  },
-  {
-    path: 'employe',
-    loadChildren: () => import('./employe/employe.module').then( m => m.EmployePageModule)
-  },
-  {
     path: 'accueil',
     loadChildren: () => import('./accueil/accueil.module').then( m => m.AccueilPageModule)
   },
