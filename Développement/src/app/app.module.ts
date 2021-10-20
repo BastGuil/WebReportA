3ce8ac4c92eb2a6b1836b8a03013b06c024c0c5b
Florian <palmier.florian@gmail.com>
2021-10-20 15:01:17 +0200

    authentification

--- Développement/src/app/app.module.ts
+++ Développement/src/app/app.module.ts
@@ -6,12 +6,32 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+import {AngularFireModule} from '@angular/fire/compat/';
+import {AngularFireDatabaseModule} from '@angular/fire/compat/database';
+import {AngularFireStorageModule} from '@angular/fire/compat/storage';
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment' ;
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCUUrS5lymfvk6B0dMxN8OD7N8y4Ev1FcI",
+  authDomain: "webreport-574b8.firebaseapp.com",
+  databaseURL: "https://webreport-574b8-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "webreport-574b8",
+  storageBucket: "webreport-574b8.appspot.com",
+  messagingSenderId: "343731566514",
+  appId: "1:343731566514:web:4298cdf113bbc4112216ab"
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp(firebaseConfig)],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+
--- Développement/src/app/connexion/connexion.page.ts
+++ Développement/src/app/connexion/connexion.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 @Component({
   selector: 'app-connexion',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./connexion.page.scss'],
 })
 export class ConnexionPage implements OnInit {
-
+  id : string="";
+  password : string="";
   constructor() { }
 
   ngOnInit() {
   }
 
+  auth(){
+      const auth = getAuth();
+    signInWithEmailAndPassword(auth, this.id + "@gmail.com", this.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user.uid);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
 }
